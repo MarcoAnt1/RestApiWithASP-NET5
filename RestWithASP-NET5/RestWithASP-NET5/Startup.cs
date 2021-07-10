ce786f32992bc6c43a9f68b042ef5e41080e3bb0
Marco Antonio de Souza Filho <54957584+MarcoAnt1@users.noreply.github.com>
2021-07-10 16:42:23 -0300

    Add connection on startup.

--- RestWithASP-NET5/RestWithASP-NET5/Startup.cs
+++ RestWithASP-NET5/RestWithASP-NET5/Startup.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RestWithASP_NET5.Model.Context;
 using RestWithASP_NET5.Services;
 using RestWithASP_NET5.Services.Implementations;
 
@@ -22,6 +24,9 @@ namespace RestWithASP_NET5
         {
 
             services.AddControllers();
+
+            var connection = Configuration["MySQLConnection:MySQLConnectionString"];
+            services.AddDbContext<MySQLContext>(options => options.UseMySql(connection, ServerVersion.AutoDetect(connection)));
 
             // Dependency Injection
             services.AddScoped<IPersonService, PersonServiceImplementation>();
