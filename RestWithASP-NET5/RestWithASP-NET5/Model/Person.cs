e99c4b318972cd8fccc8da03485b43b99067c8f2
Marco Antonio de Souza Filho <54957584+MarcoAnt1@users.noreply.github.com>
2021-07-09 21:30:20 -0300

    Add interface personal service.

--- RestWithASP-NET5/RestWithASP-NET5/Model/Person.cs
+++ RestWithASP-NET5/RestWithASP-NET5/Model/Person.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace RestWithASP_NET5.Model
+﻿namespace RestWithASP_NET5.Model
 {
     public class Person
     {
