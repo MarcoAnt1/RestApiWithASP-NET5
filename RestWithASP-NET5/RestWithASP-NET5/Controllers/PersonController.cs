f0f807d69ff8bbc0c5e57e2c4f82349da20f23cc
Marco Antonio de Souza Filho <54957584+MarcoAnt1@users.noreply.github.com>
2021-07-09 21:21:50 -0300

    Working with four verbs.

---
+++ RestWithASP-NET5/RestWithASP-NET5/Controllers/PersonController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Globalization;
+
+namespace RestWithASP_NET5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private readonly ILogger<CalculatorController> _logger;
+
+        public PersonController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            return BadRequest("Invalide input.");
+        }
+    }
+}
