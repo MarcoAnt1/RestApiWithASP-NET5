186c8f7d6ba55e2e522be150fc6246b975b03176
Marco Antonio de Souza Filho <54957584+MarcoAnt1@users.noreply.github.com>
2021-07-11 12:56:08 -0300

    Add book repository.

--- RestWithASP-NET5/RestWithASP-NET5/Model/Context/MySQLContext.cs
+++ RestWithASP-NET5/RestWithASP-NET5/Model/Context/MySQLContext.cs
@@ -9,5 +9,7 @@ namespace RestWithASP_NET5.Model.Context
         public MySqlContext(DbContextOptions<MySqlContext> options) : base(options) { }
 
         public DbSet<Person> People { get; set; }
+
+        public DbSet<Books> Books { get; set; }
     }
 }
---
+++ RestWithASP-NET5/RestWithASP-NET5/Repository/Implementations/BooksRepositoryImplementation.cs
@@ -0,0 +1,87 @@
+﻿using RestWithASP_NET5.Model;
+using RestWithASP_NET5.Model.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestWithASP_NET5.Repository.Implementations
+{
+    public class BooksRepositoryImplementation : IBooksRepository
+    {
+        private readonly MySqlContext _context;
+
+        public BooksRepositoryImplementation(MySqlContext context)
+        {
+            _context = context;
+        }
+
+        public List<Books> FindAll()
+        {
+            return _context.Books.ToList();
+        }
+
+        public Books FindById(long id)
+        {
+            return _context.Books.SingleOrDefault(b => b.Id.Equals(id));
+        }
+
+        public Books Create(Books books)
+        {
+            try
+            {
+                _context.Add(books);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return books;
+        }
+
+        public Books Update(Books books)
+        {
+            if (!Exists(books.Id))
+                return null;
+
+            var result = _context.Books.SingleOrDefault(p => p.Id.Equals(books.Id));
+            if (result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(books);
+                    _context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+
+            return books;
+        }
+
+        public void Delete(long id)
+        {
+            var result = _context.Books.SingleOrDefault(p => p.Id.Equals(id));
+            if (result != null)
+            {
+                try
+                {
+                    _context.Books.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+
+        public bool Exists(long id)
+        {
+            return _context.Books.Any(p => p.Id.Equals(id));
+        }
+    }
+}
