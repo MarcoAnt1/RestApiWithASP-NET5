cd34e8542cc3f6215626282880147d60277d8b96
Marco Antonio de Souza Filho <54957584+MarcoAnt1@users.noreply.github.com>
2021-07-11 16:27:26 -0300

    Implementing the Repository Base Structure.

---
+++ RestWithASP-NET5/RestWithASP-NET5/Repository/Generic/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using RestWithASP_NET5.Model.Base;
+using System.Collections.Generic;
+
+namespace RestWithASP_NET5.Repository
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        T Create(T item);
+
+        T Update(T item);
+
+        void Delete(long id);
+
+        T FindById(long id);
+
+        List<T> FindAll();
+
+        bool Exists(long id);
+    }
+}
