9c5ec8b39bb0c46dc69897575e8f428b0b1e8b56
Marco Antonio de Souza Filho <marco.de.souza.filho@avanade.com>
2021-07-12 17:42:53 -0300

    Creating interfaces and constants for HATEOAS.

---
+++ RestWithASP-NET5/RestWithASP-NET5/Hypermedia/Abstract/ISupportHyperMedia.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace RestWithASP_NET5.Hypermedia.Abstract
+{
+    public interface ISupportHyperMedia
+    {
+        List<HyperMediaLink> Links { get; set; }
+    }
+}
