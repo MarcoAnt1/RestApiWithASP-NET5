bae5f351825d8f7c29d0ad5d6ed4972b3a19b556
Marco Antonio de Souza Filho <54957584+MarcoAnt1@users.noreply.github.com>
2021-07-12 16:20:13 -0300

    Using json to change name.

--- RestWithASP-NET5/RestWithASP-NET5/Data/VO/PersonVO.cs
+++ RestWithASP-NET5/RestWithASP-NET5/Data/VO/PersonVO.cs
@@ -1,15 +1,21 @@
-﻿namespace RestWithASP_NET5.Data.VO
+﻿using System.Text.Json.Serialization;
+
+namespace RestWithASP_NET5.Data.VO
 {
     public class PersonVO
     {
         public int Id { get; set; }
 
+        [JsonPropertyName("Nome")]
         public string FirstName { get; set; }
 
+        [JsonPropertyName("Sobrenome")]
         public string LastName { get; set; }
 
+        [JsonPropertyName("Endereco")]
         public string Address { get; set; }
 
+        [JsonIgnore()]
         public string Gender { get; set; }
     }
 }
