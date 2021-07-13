ec1051fe9385fa0e046af67d54ebc7e87b7b3d6f
Marco Antonio de Souza Filho <marco.de.souza.filho@avanade.com>
2021-07-13 11:14:59 -0300

    Implementing hypermedialink class.

---
+++ RestWithASP-NET5/RestWithASP-NET5/Hypermedia/HyperMediaLink.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace RestWithASP_NET5.Hypermedia
+{
+    public class HyperMediaLink
+    {
+        public string Rel { get; set; }
+
+        private string href { get; set; }
+
+        public string Href
+        {
+            get
+            {
+                object _lock = new();
+                lock (_lock)
+                {
+                    StringBuilder sb = new();
+                    return sb.Replace("%2F", "/").ToString();
+                }
+            }
+            set
+            {
+                href = value;
+            }
+        }
+
+        public string Type { get; set; }
+
+        public string Action { get; set; }
+
+    }
+}
