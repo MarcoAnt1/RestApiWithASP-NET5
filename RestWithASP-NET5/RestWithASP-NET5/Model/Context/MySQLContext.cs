11a739534513e355188feee8d4a12cfedfa012b6
Marco Antonio de Souza Filho <marco.de.souza.filho@avanade.com>
2021-07-14 16:58:38 -0300

    Including User on MySQLContext.

--- RestWithASP-NET5/RestWithASP-NET5/Model/Context/MySQLContext.cs
+++ RestWithASP-NET5/RestWithASP-NET5/Model/Context/MySQLContext.cs
@@ -11,5 +11,7 @@ namespace RestWithASP_NET5.Model.Context
         public DbSet<Person> People { get; set; }
 
         public DbSet<Book> Books { get; set; }
+
+        public DbSet<User> Users { get; set; }
     }
 }
