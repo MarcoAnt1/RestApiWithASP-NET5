9f478dffd2afdf962aed535ba8c5c8e26783be68
Marco Antonio de Souza Filho <marco.de.souza.filho@avanade.com>
2021-07-14 17:28:42 -0300

    Implementing UserRepository.

---
+++ RestWithASP-NET5/RestWithASP-NET5/Repository/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using RestWithASP_NET5.Data.VO;
+using RestWithASP_NET5.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASP_NET5.Repository
+{
+    public interface IUserRepository
+    {
+        User ValidadeCredentials(UserVO user);
+    }
+}
---
+++ RestWithASP-NET5/RestWithASP-NET5/Repository/UserRepository.cs
@@ -0,0 +1,36 @@
+﻿using RestWithASP_NET5.Data.VO;
+using RestWithASP_NET5.Model;
+using RestWithASP_NET5.Model.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestWithASP_NET5.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly MySqlContext _context;
+
+        public UserRepository(MySqlContext context)
+        {
+            _context = context;
+        }
+
+        public User ValidadeCredentials(UserVO user)
+        {
+            var password = ComputeHash(user.Password, new SHA256CryptoServiceProvider());
+            return _context.Users.FirstOrDefault(u => u.UserName == user.UserName && u.Password == password);
+        }
+
+        private string ComputeHash(string input, SHA256CryptoServiceProvider algorithm)
+        {
+            byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+            byte[] hashedBytes = algorithm.ComputeHash(inputBytes);
+
+            return BitConverter.ToString(hashedBytes);
+        }
+    }
+}
