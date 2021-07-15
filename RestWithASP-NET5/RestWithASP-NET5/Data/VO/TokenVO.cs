0b6b9365d454379ee879614419728019693b4981
Marco Antonio de Souza Filho <marco.de.souza.filho@avanade.com>
2021-07-15 14:34:15 -0300

    Implementing business and vo of Login.

---
+++ RestWithASP-NET5/RestWithASP-NET5/Data/VO/TokenVO.cs
@@ -0,0 +1,24 @@
+﻿namespace RestWithASP_NET5.Data.VO
+{
+    public class TokenVO
+    {
+        public TokenVO(bool authenticated, string created, string expiraton, string accessToken, string refreshToken)
+        {
+            Authenticated = authenticated;
+            Created = created;
+            Expiraton = expiraton;
+            AccessToekn = accessToken;
+            RefreshToken = refreshToken;
+        }
+
+        public bool Authenticated { get; set; }
+
+        public string Created { get; set; }
+
+        public string Expiraton { get; set; }
+
+        public string AccessToekn { get; set; }
+
+        public string RefreshToken { get; set; }
+    }
+}
