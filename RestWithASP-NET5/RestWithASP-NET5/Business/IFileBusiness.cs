3882711d31d9765189c41f65f2547a6133c38547
Marco Antonio de Souza Filho <54957584+MarcoAnt1@users.noreply.github.com>
2021-07-17 18:00:45 -0300

    Working with files.

---
+++ RestWithASP-NET5/RestWithASP-NET5/Business/IFileBusiness.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using RestWithASP_NET5.Data.VO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RestWithASP_NET5.Business
+{
+    public interface IFileBusiness
+    {
+        public byte[] GetFile(string fileName);
+
+        public Task<FileDetailVO> SaveFileToDisk(IFormFile file);
+
+        public Task<List<FileDetailVO>> SaveFilesToDisk(IList<IFormFile> files);
+    }
+}
---
+++ RestWithASP-NET5/RestWithASP-NET5/Business/Implementations/FileBusiness.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using RestWithASP_NET5.Data.VO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace RestWithASP_NET5.Business.Implementations
+{
+    public class FileBusiness : IFileBusiness
+    {
+        private readonly string basePath;
+        private readonly IHttpContextAccessor _context;
+
+        public FileBusiness(IHttpContextAccessor context)
+        {
+            _context = context;
+            basePath = $@"{Directory.GetCurrentDirectory()}\UploadDir\";
+        }
+
+        public byte[] GetFile(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<FileDetailVO>> SaveFilesToDisk(IList<IFormFile> files)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<FileDetailVO> SaveFileToDisk(IFormFile file)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
